4bd0488a449d523f99f7fa82db739a1b3ed6ac19
indamirkhan <81577382+indamirkhan@users.noreply.github.com>
2021-11-16 18:06:24 +0530

    Create index.js

---
+++ nodejs-for-api/index.js
@@ -0,0 +1,123 @@
+//create new app for api
+
+///
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+// parse application/json
+app.use(bodyParser.json());
+
+//Create Database Connection
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud",
+});
+
+// connect to database
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL connected");
+});
+
+// creat a new Record
+app.post("/api/create", (req, res) => {
+  let data = { name: req.body.name, location: req.body.location };
+  let sql = "INSERT INTO users SET ?";
+  let query = conn.query(sql, data, (err, result) => {
+    if (err) throw err;
+    res.send(
+      JSON.stringify({
+        status: 200,
+        error: null,
+        response: "New Record is Added successfully",
+      })
+    );
+  });
+});
+
+// show all records
+app.get("/api/view", (req, res) => {
+  let sql = "SELECT * FROM users";
+  let query = conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(JSON.stringify({ status: 200, error: null, response: result }));
+  });
+});
+
+// show a single record
+app.get("/api/view/:id", (req, res) => {
+  let sql = "SELECT * FROM users WHERE id=" + req.params.id;
+  let query = conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(JSON.stringify({ status: 200, error: null, response: result }));
+  });
+});
+
+// delete the record
+app.delete("/api/delete/:id", (req, res) => {
+  let sql = "DELETE FROM users WHERE id=" + req.params.id + "";
+  let query = conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(
+      JSON.stringify({
+        status: 200,
+        error: null,
+        response: "Record deleted successfully",
+      })
+    );
+  });
+});
+
+// update the Record
+app.put("/api/update/", (req, res) => {
+  let sql =
+    "UPDATE users SET name='" +
+    req.body.name +
+    "', location='" +
+    req.body.location +
+    "' WHERE id=" +
+    req.body.id;
+  let query = conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(
+      JSON.stringify({
+        status: 200,
+        error: null,
+        response: "Record updated SuccessFully",
+      })
+    );
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server started on port 8000...");
+});
+
+//create new app for api
+
